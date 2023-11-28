f2747753d99093ad7e2a6b1373d1e924344792c8
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-28 11:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	function getFormvalue() {
+var x=document.getElementById("form1");
+let name = ""
+for (var i=0;i<x.length;i++) {
+if (x.elements[i].value!='Submit')
+name += x.elements[i].value + " "
+}
+
+alert(name.substring(0,name.length-1));
+}
   
 }
 
